48b173eab9ffb3a3a159fa0ffbb5a028fe92235e
Sh1tters <justinbestworldwide@gmail.com>
2022-10-04 23:37:17 +0200

    Creates private channel now - checks if an active private channels is already made also.

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,27 +1,51 @@
 const { ChannelType } = require('discord.js');
 const { joinVoiceChannel } = require('@discordjs/voice') //npm install @discordjs/voice
 const config = require('../config.json');
+const fs = require('fs')
 
 
 module.exports = {
     name: 'voiceStateUpdate',
     async run(oldState, newState) {
         try {
+            if (newState.channel.id == null) return;
             if(newState.channel.id == config.VH.jtc.id) {
+                var data = fs.readFileSync("./config.json")
+                var parsedData = JSON.parse(data)
+
+                // Check if user already has a voice channel
+                for(let i = 0; i< parsedData['VH']['channels'].length; i++) {
+                    if (parsedData['VH']['channels'][i]['owner_id'] == newState.guild.ownerId){ // user already has an active voice channel
+                        newState.disconnect(['An active VC of yours, has been located.'])
+                        return
+                    }
+                }
+                // Create voice channel
                 const voiceChannel = newState.guild.channels.create({
                     name: newState.member.user.username + '\'s VC',
                     type: ChannelType.GuildVoice,
                     parent: config.VH.parent
                 })
-                await Promise.resolve(voiceChannel).then((value) => {
-                    // append channel id, id of owner who created the channel and more to the config channels: []
-                });
+                await Promise.resolve(voiceChannel).then(async(value) => {
 
-                const connection = joinVoiceChannel({
-                    channelID: 0, // get the id from the config.channel.<id>
-                    guildId: newState.guild.id,
-                    adapterCreator: newState.guild.adapterCreator
-                })
+                    // Create channel object
+                    var obj = {
+                        "channel_id": value.id,
+                        "owner_id": value.guild.ownerId,
+                        "parent_id": value.parentId,
+                        "guild_id": value.guildId
+                    }
+                    parsedData['VH']['channels'].push(obj)
+
+                    await fs.writeFile("./config.json", JSON.stringify(parsedData), (err) =>{
+                        if(err) throw err;
+                    })
+
+
+                    // Connect user to the VC
+                    newState.setChannel(value.id)
+
+                });
             }
         } catch(err) {console.log(err)}
     }
